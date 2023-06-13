befd81f861ba933c29d90d3aa2ca58392e41743d
Fabrício Pinto Ferreira <pferreirafabricio@gmail.com>
2023-06-12 22:10:42 -0300

    fix typo

--- source/Boot/Config.php
+++ source/Boot/Config.php
@@ -9,4 +9,4 @@ define('CONF_API_CONTENT_TYPE', 'application/json');
 define('CONF_API_CHARSET', 'utf-8');
 define('CONF_API_ALLOW_METHODS', ["GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"]);
 define('CONF_API_MAX_AGE', '3600');
-define('CONF_API_ALLOW_HEADERS', ["Origin", "Content-Type", "Access-Controll-Allow-Headers", "Authorization"]);
+define('CONF_API_ALLOW_HEADERS', ["Origin", "Content-Type", "Access-Control-Allow-Headers", "Authorization"]);
